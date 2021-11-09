ab03f7015f45f319bdf01df832f511492dadbd8d
tú nguyễn <19110035@student.hcmute.edu.vn>
2021-11-09 14:48:40 +0700

    aa

--- QUANLYTHUVIEN/frmDangNhap.Designer.cs
+++ QUANLYTHUVIEN/frmDangNhap.Designer.cs
@@ -183,7 +183,7 @@
             // 
             this.pictureBox1.BackgroundImage = global::QUANLYTHUVIEN.Properties.Resources.library;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(451, 22);
+            this.pictureBox1.Location = new System.Drawing.Point(462, 22);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(487, 474);
             this.pictureBox1.TabIndex = 6;
